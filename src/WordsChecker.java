925e0181c6483773c3275143974a3aa64a4d20dd
Diana Bayazitova <onlydi@mail.ru>
2022-08-28 15:55:18 +0500

    Instead list.contains - map.contains. It works faster

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class WordsChecker {
-    protected static String text;
-    List<String> list = new LinkedList<>();
+    protected String text;
+    protected Set<String> set = new HashSet<>();
 
     public WordsChecker(String text) {
         this.text = text;
@@ -15,16 +12,15 @@ public class WordsChecker {
         String[] textArray = text.split(" ");
         for (int i = 0; i < textArray.length; i++) {
             if (textArray[i].contains(",") || textArray[i].contains(".")) {
-                list.add(textArray[i].substring(0, (textArray[i].length() - 1)).toLowerCase());
+                set.add(textArray[i].substring(0, (textArray[i].length() - 1)).toLowerCase());
             } else {
-                list.add(textArray[i].toLowerCase());
+                set.add(textArray[i].toLowerCase());
             }
         }
-        //System.out.println(list);
     }
 
     public boolean hasWord(String word) {
-        if (list.contains(word)) {
+        if (set.contains(word)) {
             System.out.println("Vvedennoe slovo " + word + " v texte: ");
             return true;
         } else {
